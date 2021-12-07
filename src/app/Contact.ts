144c5ff00d7f54af6301c2d651629171a78b19c7
Gouja Ameni <gouja.ameni@gmail.com>
2021-12-07 12:13:56 +0100

    add all files

---
+++ src/app/Contact.ts
@@ -0,0 +1,7 @@
+export const Contact=[
+   
+    { 'id':1,'nom':'zuckerburg','prenom':'Mark','mail':'mark@facebook.com'},
+    { 'id':2,'nom':'gates','prenom':'Bil','mail':'bil@microsoft.com'},
+    { 'id':3,'nom':'jobs','prenom':'Steeve','mail':'steeve@apple.com'}
+    
+]
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { ConvertActionBindingResult } from '@angular/compiler/src/compiler_util/expression_converter';
 import { Component } from '@angular/core';
+import { Contact } from './Contact';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,77 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'exercice4';
+  contact=Contact;
+  //etat pour  afficher et cacher les formulaires ajouter et modifier
+  etat=1;
+  //pour récuperer le contact de l'édition(edit)
+  conta;
+  a;b;
+  //ce qui est écrit dans l'input de filter 
+  label="";
+
+  filter(event)
+  {
+    
+    let tab=[];
+    this.label=this.label+event.key;
+    console.log(event.key);
+   
+    this.contact.forEach(el=>{
+      if((el.nom.includes(this.label))||(el.prenom.includes(this.label))||(el.mail.includes(this.label)))
+      {console.log(el.nom.includes(event.key));
+      
+       tab.push(el);
+      }
+      else{  
+        //suppression de l'élément 
+        tab.splice(tab.indexOf(this.label),1);
+
+      }
+    
+    })
+    this.contact=tab;
+   /* if(event.key=="Backspace")
+   {console.log('hi');
+    this.contact=Contact;
+    this.label="";
+   } */
+  
+  }  
+
+  Ajouter(form)
+  {//ajouter un nouveau contact dans le tableau
+    this.contact.push(form);
+  
+  }
+  modifier(form)
+  {//modifier un contact dans le tableau
+    //récupération de l'indice de l'élément à modifier
+    const a=this.contact.indexOf(this.conta);
+    //remplacer le contact modifié à la place de l'ancien
+    this.contact[a]=form;
+    
+  }
+  supprimer(elem)
+  {
+    //récupération de l'élément supprimé pour pouvoir faire l'annulation de suppression
+    this.a=elem;
+    //récupération de l'indexe de lélément à supprimer
+    this.b=this.contact.indexOf(elem);
+    //suppression de l'élément 
+    this.contact.splice(this.b,1);
+  }
+  edit(val)
+  {//le variable conta recoit l'élément à éditer
+    this.conta=val;
+  }
+  AnnulerSupp(){
+    //reajouter l'élément supprimé
+  this.contact.push(this.a);
+  //retour à l'état initiale
+  this.etat=1;
+
+  }
+
+
 }
